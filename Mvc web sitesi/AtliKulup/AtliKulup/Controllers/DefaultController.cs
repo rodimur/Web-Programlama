a553cdb01689214213f0023f33cd490a0819fa19
Tunahan <tunahan7567@gmail.com>
2024-03-07 02:20:44 +0300

    first commit

---
+++ Mvc web sitesi/AtliKulup/AtliKulup/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AtliKulup.Models.Entity;
+
+namespace AtliKulup.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        AtDBEntities db = new AtDBEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLBILGI.ToList();
+            return View(degerler);
+        }
+    }
+}
